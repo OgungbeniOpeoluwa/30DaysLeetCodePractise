8b3c6ddfe8b69c4d7705c5f41770a28605c4e0c1
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2024-03-16 23:13:44 +0100

    practice

--- src/MeetingRoom.java
+++ src/MeetingRoom.java
@@ -1,12 +1,130 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MeetingRoom {
 
-    public static int returnRoomsThatHeldMoreMetting(int[][] meetings, int rooms) {
-        System.out.println(Arrays.toString(Arrays.stream(meetings).flatMap(x -> Arrays.stream(x).sorted().mapToObj(int[]::new)).);
+    public static int returnRoomsThatHeldMoreMetting(int[][] meetings, int rooms){
+        boolean [] meeting = new boolean[meetings.length];
+        boolean [] room = new boolean[rooms];
+        int count = 0;
+        int [] meetingRoom = new int[rooms];
+        meetings = sort(meetings);
+        int time = 0;
+        HashMap<Integer, List<Integer>> mapRoomWithTime = new HashMap<>();
+        int end = returnTheMaximumMeetingEndingTime(meetings);
+       for(time = 0; time <  end; time++){
+           if(checkIfAllIsTrue(meeting))break;
+           int numb = returnEmptyRoom(room);
+           if(numb != -1 && checkIfTimeIsInMeeting(meetings,time)){
+               meeting = changeStatusOfMeeting(meeting,time,meetings);
+               mapRoomWithTime.put(count,List.of(returnEndTimeMeeting(meetings,time)));
+               meetingRoom[numb] +=1;
+           } else if (numb == -1 && checkIfTimeIsInMeeting(meetings,time)) {
+               int key = returnIndexWithMinimum(mapRoomWithTime);
+               int ends= returnEndTimeMeeting(meetings,time) - time;
+               System.out.println(ends);
+               List<Integer> v = mapRoomWithTime.get(key);
+               meeting = changeStatusOfMeeting(meeting,time,meetings);
+               List<Integer> lists = new ArrayList<>(v);
+               lists.add(v.getLast()+ends);
+               mapRoomWithTime.put(key,lists);
+               meetingRoom[key]+=1;
+           }
+
+       }
+        System.out.println(Arrays.toString(meeting));
+
+
+
+
         return 0;
 
+    }
+    private static HashMap<Integer,List<Integer>> checkIfMeetingHasEnded(HashMap<Integer,List<Integer>> value,int time){
+        for (int index = 0; index < value.size(); index++) {
+            for(int count = 0; count < value.get(index).size();count++){
+                if (value.get(index).get(count) == time) {
+                    List<Integer> v = new ArrayList<>(value.get(index));
+                    v.remove(count);
+                    value.put(index,v);
+                }
+            }
+        }
+            return  value;
+
+    }
+    private  static int returnIndexWithMinimum(HashMap<Integer,List<Integer>> value){
+        int min = value.get(0).getLast();
+        int key = 0;
+        for (int index = 1; index < value.size(); index++) {
+            if(value.get(index).getLast() < min){
+               min = value.get(index).getLast();
+               key = index;
+            }
+        }
+        return  key;
+
+    }
+
+    private static int returnEmptyRoom(boolean[] rooms){
+        int numb = -1;
+        for(int count = 0;count< rooms.length;count++) {
+            if(!rooms[count])numb = count;
+
+        }
+        return numb;
+    }
+
+    private static boolean checkIfTimeIsInMeeting(int [][] meeting,int time){
+        for (int [] value:meeting) {
+            if(value[0] == time)return true;
+
+        }
+        return false;
+    }
+    private static int returnEndTimeMeeting(int [][] meeting,int time){
+        int numb = 0;
+        for (int [] value:meeting) {
+            if(value[0] == time) numb = value[1];
+
+        }
+        return numb;
+    }
+    private static boolean[] changeStatusOfMeeting(boolean [] meeting,int time,int [][] meetings){
+       for(int count = 0; count < meetings.length;count++){
+           if(meetings[count][0] == time){
+               meeting[count] = true;
+           }
+       }
+        return meeting;
+    }
+
+    private static  int[][] sort(int [][] meeting){
+        for(int count = 0; count < meeting.length; count++){
+            int number = meeting[count][0];
+            for(int counter = 0; counter < meeting.length; counter++){
+                if(number<meeting[counter][0]){
+                    int [] temporary = meeting[count];
+                    meeting[count] = meeting[counter];
+                    meeting[counter] = temporary;
+                }
+            }
+        }
+        return meeting;
+    }
+    private static int returnTheMaximumMeetingEndingTime(int[][]meetings){
+        int max = 0;
+        for (int index = 0; index < meetings.length; index++) {
+            if(meetings[index][1] > max){
+                max = meetings[index][1];
+            }
+        }
+        return max;
+    }
+    private static boolean checkIfAllIsTrue(boolean[] meeting){
+        for (boolean value:meeting) {
+            if(!value)return false;
+        }
+        return true;
     }
 }
--- test/MeetingRoomTest.java
+++ test/MeetingRoomTest.java
@@ -6,7 +6,24 @@ class MeetingRoomTest {
     @Test
     public void assingMeetingRoomTest(){
         int [][] meetings = {{1,20},{2,10},{3,5},{4,9},{6,8}};
+        int rooms = 3;
+        int expected = 0;
+        assertEquals(expected,MeetingRoom.returnRoomsThatHeldMoreMetting(meetings,rooms));
+
+    }
+
+    @Test
+    public void assingMeetingToRoomTest(){
+        int [][] meetings = {{0,10},{1,5},{2,7},{3,4}};
         int rooms = 2;
+        int expected = 0;
+        assertEquals(expected,MeetingRoom.returnRoomsThatHeldMoreMetting(meetings,rooms));
+
+    }
+    @Test
+    public void assingingMeetingToAnotherRoomTest(){
+        int [][] meetings = {{18,19},{3,12},{17,19},{2,13},{7,10}};
+        int rooms = 4;
         int expected = 0;
         assertEquals(expected,MeetingRoom.returnRoomsThatHeldMoreMetting(meetings,rooms));
 
