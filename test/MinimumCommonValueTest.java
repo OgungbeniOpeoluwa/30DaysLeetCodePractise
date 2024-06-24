98c46044aceaefb3eed377c9a4de0ff4a2a6796a
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2024-06-24 21:17:43 +0100

    minimumCommonValue

--- test/MinimumCommonValueTest.java
+++ test/MinimumCommonValueTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinimumCommonValueTest {
+    @Test
+    public void twoHasCommonValueInArrayTest(){
+        int [] num1 = {7,2,3};
+        int [] num2 = {2,4};
+        int expected =  2;
+        assertEquals(expected,MinimumCommonValue.getCommon(num1,num2));
+    }
 
 }
